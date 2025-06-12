6cc09b25c19b8934d3fd6617cea3df709dd411ce
Piotr Kontny <pkontny46@gmail.com>
2025-06-12 18:01:38 +0200

    Maly Test

--- Lost_Found_Pets/settings.py
+++ Lost_Found_Pets/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lost_found_pets',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'roott',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
