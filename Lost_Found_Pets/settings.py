30161b9eb81d87399ec89c2e3658dac7e1614303
Piotr Kontny <pkontny46@gmail.com>
2025-06-12 18:03:55 +0200

    Haslo na root

--- Lost_Found_Pets/settings.py
+++ Lost_Found_Pets/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lost_found_pets',
         'USER': 'root',
-        'PASSWORD': 'roott',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
